416df19e80b7a525512c9918e292f0edc2f96c87
Scott Sullivan <scott@xrdev.com>
2022-01-03 00:26:50 -0600

    add brand icons to footer links

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,5 +1,13 @@
 import Link from 'next/link';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faGithub,
+	faTwitter,
+	faYoutube,
+	faInstagram,
+	faDiscord,
+} from '@fortawesome/free-brands-svg-icons';
 import styles from './footer.module.scss';
 
 const Footer = () => {
@@ -7,11 +15,31 @@ const Footer = () => {
 		<footer className={styles.footer}>
 			<div className={styles.social}>
 				<ul>
-					<li>Github</li>
-					<li>Twitter</li>
-					<li>Youtube</li>
-					<li>Instagram</li>
-					<li>Discord</li>
+					<li>
+						<Link href="https://github.com">
+							<FontAwesomeIcon icon={faGithub} size="2x" />
+						</Link>
+					</li>
+					<li>
+						<Link href="https://twitter.com">
+							<FontAwesomeIcon icon={faTwitter} size="2x" />
+						</Link>
+					</li>
+					<li>
+						<Link href="https://youtube.com">
+							<FontAwesomeIcon icon={faYoutube} size="2x" />
+						</Link>
+					</li>
+					<li>
+						<Link href="https://instagram.com">
+							<FontAwesomeIcon icon={faInstagram} size="2x" />
+						</Link>
+					</li>
+					<li>
+						<Link href="https://discord.com">
+							<FontAwesomeIcon icon={faDiscord} size="2x" />
+						</Link>
+					</li>
 				</ul>
 			</div>
 			<div className={styles.links}>
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import Layout from '../components/layout/layout';
 
 import '../styles/globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
 	return (
